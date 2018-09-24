dc0dec18b01ed2b1aaa1add2d797cdf8d8cc553f
kitmade <huynguyen13095@gmail.com>
2018-09-24 18:03:23 +0700

    Adding forward, back and play selected function

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -14,6 +14,7 @@ import Control from "./Controls";
 import AlbumArt from "./AlbumArt";
 import styles from "./PlayerStyles";
 import data from "../../../data";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
